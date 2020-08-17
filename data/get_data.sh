30f52c01a11959d5b5a51fdc9e5b18fd5eccb68a
hakeemtfrank <22507488+hakeemtfrank@users.noreply.github.com>
2020-08-16 22:33:12 -0700

    Update get_data.sh

--- data/get_data.sh
+++ data/get_data.sh
@@ -1,4 +1,5 @@
-# Save data as scrubbed.csv
+# Save data as scrubbed.csv.
+# Run this script from the root directory ufo-sightings/
 cd data
 wget -O scrubbed.csv  https://raw.githubusercontent.com/planetsig/ufo-reports/master/csv-data/ufo-scrubbed-geocoded-time-standardized.csv
 cd .. 
